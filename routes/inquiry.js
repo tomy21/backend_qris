7b57692b661288bb6c34ed798439a6a870d18dcc
DESKTOP-MIFG50S\Lenovo <tomy21.agung@gmail.com>
2024-01-27 16:13:20 +0700

    updated

--- routes/inquiry.js
+++ routes/inquiry.js
@@ -1,21 +1,47 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require("express");
+const router = express.Router();
+const { createServer } = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const dbConfig = require("../dbConfig");
+const response = require("../response");
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      process.env.CORS_URL_1,
+      process.env.CORS_URL_2,
+      process.env.CORS_URL_3,
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+router.use(
+  cors({
+    origin: [
+      process.env.CORS_URL_1,
+      process.env.CORS_URL_2,
+      process.env.CORS_URL_3,
+    ], // atau origin aplikasi frontend Anda
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
 const bodyParser = require("body-parser");
 const { createServer } = require("http");
 
-const app = express();
-const server = createServer(app);
+router.use(bodyParser.json());
 
-app.use(bodyParser.json());
-
-app.post("/api/inquiry", (req, res) => {
+router.post("/api/inquiry", (req, res) => {
   const apiUrl =
     "http://147.139.135.195:2101/general/Partner/InquiryTransaction";
   createProxyMiddleware({ target: apiUrl })(req, res);
 });
 
-// Atur server untuk mendengarkan port tertentu
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://127.0.0.1:${PORT}`);
-});
+module.exports = router;
