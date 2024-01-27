bc0962ab0d77c905b9a80070fc71e655f0a7507f
DESKTOP-MIFG50S\Lenovo <tomy21.agung@gmail.com>
2024-01-27 16:38:51 +0700

    updated

--- routes/inquiry.js
+++ routes/inquiry.js
@@ -1,21 +1,49 @@
 const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const http = require("http"); // Import modul http untuk membuat permintaan HTTP
 const router = express.Router();
-
-// Import middleware untuk parsing body JSON
 const bodyParser = require("body-parser");
 
-// Gunakan middleware untuk parsing body JSON
 router.use(bodyParser.json());
 
-// Handler untuk permintaan POST ke /api/inquiry
 router.post("/api/inquiry", (req, res) => {
-  // Tujuan URL eksternal yang ingin Anda hubungi
-  const apiUrl =
-    "http://147.139.135.195:2101/general/Partner/InquiryTransaction";
+  try {
+    const postData = JSON.stringify(req.body);
+    const options = {
+      hostname: "147.139.135.195",
+      port: 2101,
+      path: "/general/Partner/InquiryTransaction",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": postData.length,
+      },
+    };
 
-  // Teruskan permintaan ke server eksternal menggunakan proxy middleware
-  createProxyMiddleware({ target: apiUrl })(req, res);
+    // Buat permintaan HTTP dengan modul http
+    const request = http.request(options, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        // Kirim respons dari permintaan internal ke klien
+        res.status(response.statusCode).send(data);
+      });
+    });
+
+    request.on("error", (error) => {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
